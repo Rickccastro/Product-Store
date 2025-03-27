1fe50c520ecfa73b3d4e1f47dc8ebe40220fbfc5
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-03-26 22:37:05 -0300

    Feat: Adicionando card para mostrar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,15 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/products.interface';
+
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent implements OnInit{
-  products: any[] = [];
+  products: Product[] = [];
   productsService = inject(ProductsService);
 
   ngOnInit(): void {
