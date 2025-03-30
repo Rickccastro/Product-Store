ca0f7f997e581f18b625705b38b917dfcd75f3d7
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-03-30 17:43:45 -0300

    Refactor: Adicionei configuracoes da snackbar no app config

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -31,11 +31,7 @@ export class CreateComponent{
         {
           title: this.form.controls.title.value,
       }).subscribe((data)=>{
-        this.matSnackBar.open('Produto cadastrado com sucesso!','Ok',{
-          duration: 3000,
-          horizontalPosition: 'right',
-          verticalPosition: 'top',
-        });
+        this.matSnackBar.open('Produto cadastrado com sucesso!','Ok')
         this.router.navigateByUrl('/');
       });
     }    
