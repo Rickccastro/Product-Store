df3e810485055fa1156e76863c1d702c2c670ad2
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-03-30 23:12:19 -0300

    Refactor: cria componente de formulario para melhorar a reusabilidade

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,43 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
-import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { FormComponent } from '../../shared/components/form/form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [MatButtonModule,MatInputModule,MatFormFieldModule,ReactiveFormsModule],
+  imports: [FormComponent],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
+  styleUrl: './edit.component.css',
 })
 export class EditComponent {
-    productsService = inject(ProductsService);
-      matSnackBar = inject(MatSnackBar);
-      router = inject(Router)
-      product: Product = inject(ActivatedRoute).snapshot.data['product'];
+  productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+  product: Product = inject(ActivatedRoute).snapshot.data['product'];
 
-      form = new FormGroup(
-        {
-          title : new FormControl<string>(this.product.title,{
-            nonNullable : true,
-            validators: Validators.required}),
-        }
-      );    
-  
-      onSubmit() 
-      {
-        this.productsService.atualizarProduto(this.product.id,
-          {
-            
-            title: this.form.controls.title.value,
-        }).subscribe(()=>{
-          this.matSnackBar.open('Produto atualizado com sucesso!','Ok')
-          this.router.navigateByUrl('/');
-        });
-      }    
+  onSubmit(product: Product) {
+    this.productsService
+      .atualizarProduto(this.product.id, product)
+      .subscribe(() => {
+        this.matSnackBar.open('Produto atualizado com sucesso!', 'Ok');
+        this.router.navigateByUrl('/');
+      });
+  }
 }
